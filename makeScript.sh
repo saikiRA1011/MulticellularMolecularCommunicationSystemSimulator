de732e14f0617ceb7a56307476c9cb870dcef8fd
saikiRA1011 <a19tn017@st.osaka-cu.ac.jp>
2022-04-20 17:07:43 +0900

    makeScript is updated

--- makeScript.sh
+++ makeScript.sh
@@ -3,16 +3,7 @@
 rm ./result/*
 rm ./image/*
 
-g++ SimMain.cpp -o SimMain -std=c++20 -Wall -Wextra
-
-if [ $? ]; then
-    echo "Compilation Success"
-else
-    echo "Compilation Failed"
-    exit 1
-fi
-
-./SimMain
+g++ SimMain.cpp -o SimMain -std=c++20 -Wall -Wextra && echo "SimMain compiled" && ./SimMain
 
 python3 create_image.py
 python3 img2video.py
