0f4e98a06721564e459ca3193830fc38974399d9
saikiRA1011 <a19tn017@st.osaka-cu.ac.jp>
2022-05-25 14:36:13 +0900

    use openMP

--- makeScript.sh
+++ makeScript.sh
@@ -3,7 +3,7 @@
 rm ./result/*
 rm ./image/*
 
-g++ SimMain.cpp -o SimMain -std=c++20 -Wall -Wextra && echo "SimMain compiled" && ./SimMain
+g++-11 SimMain.cpp -o SimMain -std=c++20 -Wall -Wextra -fopenmp && echo "SimMain compiled" && ./SimMain
 
 python3 create_image.py
 python3 img2video.py
