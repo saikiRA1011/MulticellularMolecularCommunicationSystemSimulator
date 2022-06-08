5b7b27205fbe1bb9f628c5262df38fadaeb83d6b
saikiRA1011 <a19tn017@st.osaka-cu.ac.jp>
2022-06-08 16:57:16 +0900

    delete makeScript.sh

--- makeScript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm ./result/*
-rm ./image/*
-
-g++-11 SimMain.cpp -o SimMain -std=c++20 -Wall -Wextra -fopenmp && echo "SimMain compiled" && ./SimMain
-
-python3 create_image.py
-python3 img2video.py
