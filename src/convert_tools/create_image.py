a8f4f7211e422a0a1a1f42a1c833a38c1154e008
Takanori Saiki <sc23501o@st.omu.ac.jp>
2024-12-14 01:33:19 +0900

    fix convert tool

--- src/convert_tools/create_image.py
+++ src/convert_tools/create_image.py
@@ -40,7 +40,8 @@ for file in files:
         r = float(cell[8])*scale
         adhere_cells_str = cell[10]
 
-        cell_objs.append((cell_type, x, y, r, adhere_cells_str))
+        if adhere_cells_str not in ['_', '']:
+            cell_objs.append((cell_type, x, y, r, adhere_cells_str))
 
         if x < 0 or x > IMAGE_LEN or y < 0 or y > IMAGE_LEN:
             print('out of range')
